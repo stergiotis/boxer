c4bd7766f7524f393fbf5dbc063e9fbf18cfbc72
Panos Stergiotis <p@stergiotis.ch>
2024-02-24 15:15:22 +0100

    fix: checkbox unitialized value

--- public/imzero/imgui/checkbox.idl.go
+++ public/imzero/imgui/checkbox.idl.go
@@ -4,13 +4,14 @@ package imgui
 
 func Checkbox(label string, state Tristate) (checked Tristate, clicked bool) {
 	_ = `
-       bool v = state > 0;
+         bool v = state > 0;
          if(state == 0) {
+              checked = state;
               ImGui::PushItemFlag(ImGuiItemFlags_MixedValue, true);
               clicked = ImGui::Checkbox(label, &v);
               ImGui::PopItemFlag();
               if(clicked) {
-                      checked = 1;
+                 checked = 1;
               }
          } else {
               clicked = ImGui::Checkbox(label, &v);
