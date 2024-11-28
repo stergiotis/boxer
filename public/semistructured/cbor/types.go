0781101ab574b5b5121af08fb286b05515143961
Panos Stergiotis <p@stergiotis.ch>
2024-11-28 21:58:16 +0100

    feat: subdivide interface

--- public/semistructured/cbor/types.go
+++ public/semistructured/cbor/types.go
@@ -5,21 +5,33 @@ import (
 	"time"
 )
 
-type BasicEncoder interface {
+type PrimitiveEncoder interface {
 	EncodeUint(val uint64) (int, error)
 	EncodeInt(val int64) (int, error)
 	EncodeByteSlice(val []byte) (int, error)
-	EncodeCborPayload(val []byte) (int, error)
 	EncodeString(val string) (int, error)
 	EncodeBool(val bool) (int, error)
 	EncodeFloat32(val float32) (int, error)
 	EncodeFloat64(val float64) (int, error)
 	EncodeTimeUTC(val time.Time) (int, error)
+	EncodeNil() (int, error)
+}
+type DefiniteContainerEncoder interface {
 	EncodeArrayDefinite(len uint64) (int, error)
 	EncodeMapDefinite(len uint64) (int, error)
-	EncodeNil() (int, error)
+}
+type ResetableEncoder interface {
 	Reset()
 	SetWriter(dest EncoderWriter)
+}
+type CborPayloadEncoder interface {
+	EncodeCborPayload(val []byte) (int, error)
+}
+type BasicEncoder interface {
+	PrimitiveEncoder
+	DefiniteContainerEncoder
+	ResetableEncoder
+	CborPayloadEncoder
 }
 type IndefiniteContainerEncoder interface {
 	EncodeMapIndefinite() (int, error)
