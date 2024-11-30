7f123cb47fb1497d69069e0113b2a5c85171b566
Panos Stergiotis <p@stergiotis.ch>
2024-11-30 19:38:00 +0100

    feat: splicing reader

---
+++ public/ea/splicingreader.go
@@ -0,0 +1,58 @@
+package ea
+
+import (
+	"github.com/stergiotis/boxer/public/observability/eh"
+	"io"
+)
+
+type SplicingReader struct {
+	primary   io.Reader
+	secondary io.Reader
+}
+
+func NewSplicingReader(primary io.Reader) *SplicingReader {
+	return &SplicingReader{
+		primary:   primary,
+		secondary: nil,
+	}
+}
+
+var ErrSplicingReaderAlreadySet = eh.Errorf("splicing reader is already set")
+
+func (inst *SplicingReader) SpliceReader(r io.Reader) (err error) {
+	if inst.secondary != nil {
+		err = ErrSplicingReaderAlreadySet
+		return
+	}
+	inst.secondary = r
+	return
+}
+func (inst *SplicingReader) IsSpliceReaderSet() bool {
+	return inst.secondary != nil
+}
+func (inst *SplicingReader) Reset() {
+	inst.secondary = nil
+}
+
+func (inst *SplicingReader) Read(p []byte) (n int, err error) {
+	sec := inst.secondary
+	if sec != nil {
+		n, err = sec.Read(p)
+		if err == nil {
+			return
+		} else if err == io.EOF {
+			inst.secondary = nil
+			if n == 0 {
+				return inst.primary.Read(p)
+			} else {
+				err = nil
+				return
+			}
+		} else {
+			return
+		}
+	}
+	return inst.primary.Read(p)
+}
+
+var _ io.Reader = (*SplicingReader)(nil)
---
+++ public/ea/splicingreader_test.go
@@ -0,0 +1,32 @@
+package ea
+
+import (
+	"github.com/stretchr/testify/require"
+	"io"
+	"strings"
+	"testing"
+)
+
+func TestNewSplicingReader(t *testing.T) {
+	{
+		buf0 := strings.NewReader("world!")
+		b, err := io.ReadAll(NewSplicingReader(buf0))
+		require.NoError(t, err)
+		require.Equal(t, "world!", string(b))
+	}
+
+	{
+		buf0 := strings.NewReader("world!")
+		buf1 := strings.NewReader("hello ")
+		sr := NewSplicingReader(buf0)
+		err := sr.SpliceReader(buf1)
+		require.NoError(t, err)
+		err = sr.SpliceReader(buf1)
+		require.ErrorIs(t, ErrSplicingReaderAlreadySet, err)
+
+		var b []byte
+		b, err = io.ReadAll(sr)
+		require.NoError(t, err)
+		require.Equal(t, "hello world!", string(b))
+	}
+}
