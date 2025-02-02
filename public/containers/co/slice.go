8013cc8a59f04299b3ee8aa3edd8e8abe45106e6
Panos Stergiotis <p@stergiotis.ch>
2025-02-02 13:38:37 +0100

    feat: added function

--- public/containers/co/slice.go
+++ public/containers/co/slice.go
@@ -51,6 +51,25 @@ func IterateSliceGroupedFunc[K any, V any](sortedSliceKeys []K, coSliceVals []V,
 	}
 }
 
+func MergeSliceSorted[K any, V any](sortedSliceReadIn []K, coSliceWriteIn []V, key K, val V, cmpKey func(K, K) int, merge func(old V, new V) V) (idx int, existed bool, sortedSliceRead []K, coSliceWrite []V) {
+	sortedSliceRead = sortedSliceReadIn
+	coSliceWrite = coSliceWriteIn
+	idx, existed = slices.BinarySearchFunc(sortedSliceRead, key, cmpKey)
+	if existed {
+		coSliceWrite[idx] = merge(coSliceWrite[idx], val)
+	} else {
+		var dummyV V
+		var dummyK K
+		coSliceWrite = append(coSliceWrite, dummyV)
+		copy(coSliceWrite[idx+1:], coSliceWrite[idx:])
+		coSliceWrite[idx] = val
+
+		sortedSliceRead = append(sortedSliceRead, dummyK)
+		copy(sortedSliceRead[idx+1:], sortedSliceRead[idx:])
+		sortedSliceRead[idx] = key
+	}
+	return
+}
 func InsertSliceSortedFunc[K any, V any](sortedSliceReadIn []K, coSliceWriteIn []V, key K, val V, cmpKey func(K, K) int) (idx int, existed bool, sortedSliceRead []K, coSliceWrite []V) {
 	sortedSliceRead = sortedSliceReadIn
 	coSliceWrite = coSliceWriteIn
