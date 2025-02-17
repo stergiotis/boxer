aed37cb6a9813021c87a0cb631c45dec666809ca
stergiotis <p@stergiotis.ch>
2025-02-17 21:43:17 +0200

    feat: co-slice sort

--- public/containers/co/slice.go
+++ public/containers/co/slice.go
@@ -4,7 +4,79 @@ import (
 	"cmp"
 	"iter"
 	"slices"
+	"sort"
 )
+
+type coSorter[K cmp.Ordered] struct {
+	swap  func(i int, j int)
+	slice []K
+}
+
+func (inst *coSorter[T]) Len() int {
+	return len(inst.slice)
+}
+
+func (inst *coSorter[T]) Less(i, j int) bool {
+	slice := inst.slice
+	return slice[i] < slice[j]
+}
+
+func (inst *coSorter[T]) Swap(i, j int) {
+	inst.swap(i, j)
+	slice := inst.slice
+	slice[j], slice[i] = slice[i], slice[j]
+}
+
+var _ sort.Interface = (*coSorter[int])(nil)
+
+type coSorterReverse[K cmp.Ordered] struct {
+	swap  func(i int, j int)
+	slice []K
+}
+
+func (inst *coSorterReverse[T]) Len() int {
+	return len(inst.slice)
+}
+
+func (inst *coSorterReverse[T]) Less(i, j int) bool {
+	slice := inst.slice
+	return slice[i] > slice[j]
+}
+
+func (inst *coSorterReverse[T]) Swap(i, j int) {
+	inst.swap(i, j)
+	slice := inst.slice
+	slice[j], slice[i] = slice[i], slice[j]
+}
+
+var _ sort.Interface = (*coSorterReverse[int])(nil)
+
+func CoSortSlices[K cmp.Ordered](slice []K, swap func(i int, j int)) {
+	switch len(slice) {
+	case 0, 1:
+		return
+	}
+	if swap == nil {
+		slices.Sort(slice)
+	} else {
+		s := &coSorter[K]{
+			slice: slice,
+			swap:  swap,
+		}
+		sort.Sort(s)
+	}
+}
+func CoSortSlicesReverse[K cmp.Ordered](slice []K, swap func(i int, j int)) {
+	switch len(slice) {
+	case 0, 1:
+		return
+	}
+	s := &coSorterReverse[K]{
+		slice: slice,
+		swap:  swap,
+	}
+	sort.Sort(s)
+}
 
 func IterateSliceGrouped[K comparable, V any](sortedSliceKeys []K, coSliceVals []V) iter.Seq2[K, []V] {
 	return func(yield func(K, []V) bool) {
--- public/containers/co/slice_test.go
+++ public/containers/co/slice_test.go
@@ -1,8 +1,9 @@
 package co
 
 import (
-	"github.com/stretchr/testify/require"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func cmpInt(i int, i2 int) int {
@@ -16,14 +17,14 @@ func cmpInt(i int, i2 int) int {
 }
 
 func TestIterateSliceGrouped(t *testing.T) {
-	for k, vs := range IterateSliceGrouped([]int{1}, []int{-1}, cmpInt) {
+	for k, vs := range IterateSliceGroupedFunc([]int{1}, []int{-1}, cmpInt) {
 		require.Equal(t, 1, k)
 		require.EqualValues(t, []int{-1}, vs)
 	}
 
 	{
 		i := 1
-		for k, vs := range IterateSliceGrouped([]int{1, 2}, []int{-1, -2}, cmpInt) {
+		for k, vs := range IterateSliceGroupedFunc([]int{1, 2}, []int{-1, -2}, cmpInt) {
 			require.Equal(t, i, k)
 			require.EqualValues(t, []int{-i}, vs)
 			i++
@@ -31,7 +32,7 @@ func TestIterateSliceGrouped(t *testing.T) {
 	}
 	{
 		i := 1
-		for k, vs := range IterateSliceGrouped([]int{1, 2, 3}, []int{-1, -2, -3}, cmpInt) {
+		for k, vs := range IterateSliceGroupedFunc([]int{1, 2, 3}, []int{-1, -2, -3}, cmpInt) {
 			require.Equal(t, i, k)
 			require.EqualValues(t, []int{-i}, vs)
 			i++
@@ -39,7 +40,7 @@ func TestIterateSliceGrouped(t *testing.T) {
 	}
 	{
 		i := 0
-		for k, vs := range IterateSliceGrouped([]int{1, 1, 2, 2, 3, 3}, []int{-1, -2, -3, -4, -5, -6}, cmpInt) {
+		for k, vs := range IterateSliceGroupedFunc([]int{1, 1, 2, 2, 3, 3}, []int{-1, -2, -3, -4, -5, -6}, cmpInt) {
 			require.Equal(t, i+1, k)
 			require.EqualValues(t, [][]int{{-1, -2}, {-3, -4}, {-5, -6}}[i], vs)
 			i++
@@ -47,7 +48,7 @@ func TestIterateSliceGrouped(t *testing.T) {
 	}
 	{
 		i := 0
-		for k, vs := range IterateSliceGrouped([]int{1, 1, 1, 1, 1, 3}, []int{-1, -2, -3, -4, -5, -6}, cmpInt) {
+		for k, vs := range IterateSliceGroupedFunc([]int{1, 1, 1, 1, 1, 3}, []int{-1, -2, -3, -4, -5, -6}, cmpInt) {
 			require.Equal(t, []int{1, 3}[i], k)
 			require.EqualValues(t, [][]int{{-1, -2, -3, -4, -5}, {-6}}[i], vs)
 			i++
@@ -55,7 +56,7 @@ func TestIterateSliceGrouped(t *testing.T) {
 	}
 	{
 		i := 0
-		for k, vs := range IterateSliceGrouped([]int{1, 2, 2, 2, 2, 2}, []int{-1, -2, -3, -4, -5, -6}, cmpInt) {
+		for k, vs := range IterateSliceGroupedFunc([]int{1, 2, 2, 2, 2, 2}, []int{-1, -2, -3, -4, -5, -6}, cmpInt) {
 			require.Equal(t, []int{1, 2}[i], k)
 			require.EqualValues(t, [][]int{{-1}, {-2, -3, -4, -5, -6}}[i], vs)
 			i++
@@ -63,10 +64,46 @@ func TestIterateSliceGrouped(t *testing.T) {
 	}
 	{
 		i := 0
-		for k, vs := range IterateSliceGrouped([]int{1, 2, 2, 2, 2, 3}, []int{-1, -2, -3, -4, -5, -6}, cmpInt) {
+		for k, vs := range IterateSliceGroupedFunc([]int{1, 2, 2, 2, 2, 3}, []int{-1, -2, -3, -4, -5, -6}, cmpInt) {
 			require.Equal(t, []int{1, 2, 3}[i], k)
 			require.EqualValues(t, [][]int{{-1}, {-2, -3, -4, -5}, {-6}}[i], vs)
 			i++
 		}
 	}
+}
+
+func TestCoSortSlices(t *testing.T) {
+	a := []int{4, 1, 2, 3}
+	b := []string{"4", "1", "2", "3"}
+	CoSortSlices(a, func(i int, j int) {
+		b[j], b[i] = b[i], b[j]
+	})
+	require.Equal(t, []int{1, 2, 3, 4}, a)
+	require.Equal(t, []string{"1", "2", "3", "4"}, b)
+	CoSortSlices(a[:0], func(i int, j int) {
+		require.Fail(t, "should never get here")
+	})
+	CoSortSlices(a[:1], func(i int, j int) {
+		b[j], b[i] = b[i], b[j]
+	})
+	require.Equal(t, []int{1}, a[:1])
+	require.Equal(t, []string{"1"}, b[:1])
+}
+func TestCoSortSlicesReverse(t *testing.T) {
+	a := []int{4, 1, 2, 3}
+	b := []string{"4", "1", "2", "3"}
+	CoSortSlicesReverse(a, func(i int, j int) {
+		b[j], b[i] = b[i], b[j]
+	})
+	require.Equal(t, []int{4, 3, 2, 1}, a)
+	require.Equal(t, []string{"4", "3", "2", "1"}, b)
+
+	CoSortSlicesReverse(a[:0], func(i int, j int) {
+		require.Fail(t, "should never get here")
+	})
+	CoSortSlicesReverse(a[:1], func(i int, j int) {
+		b[j], b[i] = b[i], b[j]
+	})
+	require.Equal(t, []int{4}, a[:1])
+	require.Equal(t, []string{"4"}, b[:1])
 }
