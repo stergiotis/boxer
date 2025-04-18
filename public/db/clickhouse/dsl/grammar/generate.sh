58ba8a9bdd53433614125bef2ad13f37257acc0a
stergiotis <p@stergiotis.ch>
2025-04-18 22:39:34 +0200

    fix: minor fixes

--- public/db/clickhouse/dsl/grammar/generate.sh
+++ public/db/clickhouse/dsl/grammar/generate.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ev
+# shellcheck disable=SC2128
 here=$(dirname "$(readlink -f "$BASH_SOURCE")")
 cd "$here"
 rm -f *.out.*
 export ANTLR4_TOOLS_ANTLR_VERSION="4.13.2" 
 antlr4 -Werror -Dlanguage=Go -visitor -no-listener -package grammar -o . ClickHouseLexer.g4 ClickHouseParser.g4
-rename "s/\.go/.out.go/" *.go
-rm -rf gen
+rename .go .out.go ./*.go
