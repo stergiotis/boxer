4b0cffbc49a0e2423f4ef18d4697264561c90360
stergiotis <p@stergiotis.ch>
2025-08-05 21:35:16 +0200

    feat: docgen cli

--- public/docgen/docflags.go
+++ public/docgen/docflags.go
@@ -1,12 +1,15 @@
 package docgen
 
 import (
+	"bytes"
 	"os"
 	"path/filepath"
 	"slices"
 	"strings"
 
 	md "github.com/nao1215/markdown"
+	"github.com/stergiotis/boxer/public/observability/eh"
+	"github.com/stergiotis/pebble2impl/public/boxerstaging/cliutils"
 	"github.com/urfave/cli/v2"
 )
 
@@ -33,4 +36,77 @@ var DocFlags = []cli.Flag{
 			return nil
 		},
 	},
+}
+
+func NewDocCli() *cli.Command {
+	syntaxHighlightFlag, syntaxHighlightFunc := cliutils.BuildEnumStringFlag([]md.SyntaxHighlight{
+		md.SyntaxHighlightNone,
+		md.SyntaxHighlightText,
+		md.SyntaxHighlightAPIBlueprint,
+		md.SyntaxHighlightShell,
+		md.SyntaxHighlightGo,
+		md.SyntaxHighlightJSON,
+		md.SyntaxHighlightYAML,
+		md.SyntaxHighlightXML,
+		md.SyntaxHighlightHTML,
+		md.SyntaxHighlightCSS,
+		md.SyntaxHighlightJavaScript,
+		md.SyntaxHighlightTypeScript,
+		md.SyntaxHighlightSQL,
+		md.SyntaxHighlightC,
+		md.SyntaxHighlightCSharp,
+		md.SyntaxHighlightCPlusPlus,
+		md.SyntaxHighlightJava,
+		md.SyntaxHighlightKotlin,
+		md.SyntaxHighlightPHP,
+		md.SyntaxHighlightPython,
+		md.SyntaxHighlightRuby,
+		md.SyntaxHighlightSwift,
+		md.SyntaxHighlightScala,
+		md.SyntaxHighlightRust,
+		md.SyntaxHighlightObjectiveC,
+		md.SyntaxHighlightPerl,
+		md.SyntaxHighlightLua,
+		md.SyntaxHighlightDart,
+		md.SyntaxHighlightClojure,
+		md.SyntaxHighlightGroovy,
+		md.SyntaxHighlightR,
+		md.SyntaxHighlightHaskell,
+		md.SyntaxHighlightErlang,
+		md.SyntaxHighlightElixir,
+		md.SyntaxHighlightOCaml,
+		md.SyntaxHighlightJulia,
+		md.SyntaxHighlightScheme,
+		md.SyntaxHighlightFSharp,
+		md.SyntaxHighlightCoffeeScript,
+		md.SyntaxHighlightVBNet,
+		md.SyntaxHighlightTeX,
+		md.SyntaxHighlightDiff,
+		md.SyntaxHighlightApache,
+		md.SyntaxHighlightDockerfile,
+		md.SyntaxHighlightMermaid,
+	}, md.SyntaxHighlightNone, "syntaxHighlight")
+	return &cli.Command{
+		Name: "doc",
+		Subcommands: []*cli.Command{
+			{
+				Name: "markdown",
+				Subcommands: []*cli.Command{
+					{
+						Name:  "codeblock",
+						Usage: "wraps text from stdin in a markdown code block",
+						Flags: []cli.Flag{syntaxHighlightFlag},
+						Action: func(context *cli.Context) error {
+							buf := bytes.NewBuffer(make([]byte, 0, 4*4096))
+							_, err := buf.ReadFrom(os.Stdin)
+							if err != nil {
+								return eh.Errorf("unable to read from stdin: %w", err)
+							}
+							return md.NewMarkdown(os.Stdout).CodeBlocks(syntaxHighlightFunc(context), buf.String()).Build()
+						},
+					},
+				},
+			},
+		},
+	}
 }
