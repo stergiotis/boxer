ddefb0f655df728e96f216535f5a16394de2083e
stergiotis <p@stergiotis.ch>
2025-08-08 20:05:56 +0200

    feat: use go tool, use tsparse

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,8 @@
 set -ev
 here=$(dirname "$(readlink -f "$BASH_SOURCE")")
 cd "$here"
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.62.2
-go install gotest.tools/gotestsum@latest
-go install github.com/incu6us/goimports-reviser/v3@latest
-go install github.com/dkorunic/betteralign/cmd/betteralign@latest
+go get -tool github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+go get -tool gotest.tools/gotestsum@latest
+go get -tool github.com/incu6us/goimports-reviser/v3@latest
+go get -tool github.com/dkorunic/betteralign/cmd/betteralign@latest
+go get -tool github.com/mfridman/tparse@latest
