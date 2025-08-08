fc5fe06a92eb969ac098666779c005634b3665e9
stergiotis <p@stergiotis.ch>
2025-08-08 21:49:53 +0200

    fix: path

--- scripts/ci/install.sh
+++ scripts/ci/install.sh
@@ -4,5 +4,5 @@ here=$(dirname "$(readlink -f "$BASH_SOURCE")")
 cd "$here"
 go get -tool github.com/mfridman/tparse@latest
 go get -tool go.uber.org/nilaway/cmd/nilaway@latest
-go get -tool cyclonedx/cyclonedx/cyclonedx-gomod@latest
+go get -tool github.com/CycloneDX/cyclonedx-gomod/cmd/cyclonedx-gomod@latest
 go get -tool golang.org/x/vuln/cmd/govulncheck@latest
