2b282018b48047fb73e58e338efcfdcc1c8a413c
stergiotis <p@stergiotis.ch>
2025-08-09 20:20:09 +0200

    feat: binary search kv container

---
+++ public/containers/binarysearchkv.go
@@ -0,0 +1,201 @@
+package containers
+
+import (
+	"sort"
+
+	"iter"
+	"slices"
+
+	"github.com/stergiotis/boxer/public/containers/co"
+	"github.com/stergiotis/boxer/public/observability/eh"
+)
+
+type BinarySearchGrowingKV[K any, V any] struct {
+	keys      []K
+	vals      []V
+	cmpKey    func(a K, b K) int
+	sorted    bool
+	compacted bool
+}
+
+func (inst *BinarySearchGrowingKV[K, V]) Len() int {
+	return len(inst.keys)
+}
+
+func (inst *BinarySearchGrowingKV[K, V]) Less(i, j int) bool {
+	keys := inst.keys
+	return inst.cmpKey(keys[i], keys[j]) < 0
+}
+
+func (inst *BinarySearchGrowingKV[K, V]) Swap(i, j int) {
+	keys := inst.keys
+	vals := inst.vals
+	keys[j], keys[i] = keys[i], keys[j]
+	vals[j], vals[i] = vals[i], vals[j]
+}
+
+var ErrCmpKeyIsNil = eh.Errorf("cmpKey function is nil")
+
+func NewBinarySearchGrowingKV[K any, V any](estSize int, cmpKey func(a K, b K) int) (inst *BinarySearchGrowingKV[K, V], err error) {
+	if cmpKey == nil {
+		err = ErrCmpKeyIsNil
+		return
+	}
+	inst = &BinarySearchGrowingKV[K, V]{
+		keys:      make([]K, 0, estSize),
+		vals:      make([]V, 0, estSize),
+		cmpKey:    cmpKey,
+		sorted:    true,
+		compacted: true,
+	}
+	return
+}
+
+func (inst *BinarySearchGrowingKV[K, V]) ensureSorted() {
+	if !inst.sorted {
+		sort.Stable(inst)
+		inst.sorted = true
+	}
+	if !inst.compacted {
+		// FIXME this is ugly and most likely slow, rewrite compact to keep last instance
+		slices.Reverse(inst.keys)
+		slices.Reverse(inst.vals)
+		inst.compactOldestWins()
+		slices.Reverse(inst.keys)
+		slices.Reverse(inst.vals)
+		inst.compacted = true
+	}
+}
+
+// compactOldestWins adopted from go stdlib slices.Compact()
+func (inst *BinarySearchGrowingKV[K, V]) compactOldestWins() {
+	keys := inst.keys
+	vals := inst.vals
+	if len(keys) < 2 {
+		return
+	}
+	cmp := inst.cmpKey
+	for k := 1; k < len(keys); k++ {
+		if cmp(keys[k], keys[k-1]) != 0 {
+			continue
+		}
+
+		keys2 := keys[k:]
+		vals2 := vals[k:]
+		for k2 := 1; k2 < len(keys2); k2++ {
+			if cmp(keys2[k2], keys2[k2-1]) != 0 {
+				keys[k] = keys2[k2]
+				vals[k] = vals2[k2] // Stable sorted, keeps last value
+				k++
+			}
+		}
+
+		clear(keys[k:])
+		clear(vals[k:])
+		inst.keys = keys[:k]
+		inst.vals = vals[:k]
+		return
+	}
+}
+
+func (inst *BinarySearchGrowingKV[K, V]) compactYoungestWins() {
+	slices.Reverse(inst.keys)
+	slices.Reverse(inst.vals)
+	inst.compactOldestWins()
+	slices.Reverse(inst.keys)
+	slices.Reverse(inst.vals)
+}
+
+func (inst *BinarySearchGrowingKV[K, V]) Has(key K) (has bool) {
+	inst.ensureSorted()
+	_, has = slices.BinarySearchFunc(inst.keys, key, inst.cmpKey)
+	return
+}
+
+func (inst *BinarySearchGrowingKV[K, V]) Get(key K) (val V, has bool) {
+	inst.ensureSorted()
+	var idx int
+	idx, has = slices.BinarySearchFunc(inst.keys, key, inst.cmpKey)
+	if has {
+		val = inst.vals[idx]
+	}
+	return
+}
+
+func (inst *BinarySearchGrowingKV[K, V]) GetDefault(key K, defaultV V) (val V) {
+	inst.ensureSorted()
+	idx, has := slices.BinarySearchFunc(inst.keys, key, inst.cmpKey)
+	if has {
+		val = inst.vals[idx]
+	} else {
+		val = defaultV
+	}
+	return
+}
+func (inst *BinarySearchGrowingKV[K, V]) MergeValue(key K, val V, merge func(old V, new V) V) (existed bool) {
+	inst.ensureSorted()
+	_, existed, inst.keys, inst.vals = co.MergeSliceSorted(inst.keys, inst.vals, key, val, inst.cmpKey, merge)
+	return
+}
+
+func (inst *BinarySearchGrowingKV[K, V]) UpsertSingle(key K, val V) (existed bool) {
+	inst.ensureSorted()
+	_, existed, inst.keys, inst.vals = co.InsertSliceSortedFunc(inst.keys, inst.vals, key, val, inst.cmpKey)
+	return
+}
+
+// UpsertBatch last write wins
+func (inst *BinarySearchGrowingKV[K, V]) UpsertBatch(key K, val V) {
+	inst.keys = append(inst.keys, key)
+	inst.vals = append(inst.vals, val)
+	inst.compacted = false
+	inst.sorted = false
+}
+
+func (inst *BinarySearchGrowingKV[K, V]) Pairs() iter.Seq2[K, V] {
+	inst.ensureSorted()
+	return func(yield func(K, V) bool) {
+		keys := inst.keys
+		vals := inst.vals
+		for i, k := range keys {
+			if !yield(k, vals[i]) {
+				return
+			}
+		}
+	}
+}
+
+func (inst *BinarySearchGrowingKV[K, V]) Keys() iter.Seq[K] {
+	inst.ensureSorted()
+	return func(yield func(K) bool) {
+		keys := inst.keys
+		for _, k := range keys {
+			if !yield(k) {
+				return
+			}
+		}
+	}
+}
+
+func (inst *BinarySearchGrowingKV[K, V]) Values() iter.Seq[V] {
+	inst.ensureSorted()
+	return func(yield func(V) bool) {
+		vals := inst.vals
+		for _, v := range vals {
+			if !yield(v) {
+				return
+			}
+		}
+	}
+}
+
+func (inst *BinarySearchGrowingKV[K, V]) Reset() {
+	inst.sorted = true
+	inst.compacted = true
+	clear(inst.keys)
+	clear(inst.vals)
+	inst.keys = inst.keys[:0]
+	inst.vals = inst.vals[:0]
+}
+
+var _ sort.Interface = (*BinarySearchGrowingKV[any, any])(nil)
---
+++ public/containers/binarysearchkv_test.go
@@ -0,0 +1,43 @@
+package containers
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewBinarySearchGrowingKV(t *testing.T) {
+	dict, err := NewBinarySearchGrowingKV[[]byte, string](128, bytes.Compare)
+	require.NoError(t, err)
+	require.Equal(t, 0, dict.Len())
+	require.False(t, dict.Has([]byte("k1")))
+	dict.UpsertSingle([]byte("k1"), "v1")
+	require.True(t, dict.Has([]byte("k1")))
+	v, has := dict.Get([]byte("k1"))
+	require.True(t, has)
+	require.Equal(t, "v1", v)
+	v, has = dict.Get([]byte("k2"))
+	require.False(t, has)
+	require.Equal(t, "", v)
+	v = dict.GetDefault([]byte("k2"), "notfound")
+	require.False(t, has)
+	require.Equal(t, "notfound", v)
+
+	dict.UpsertSingle([]byte("k0"), "v0")
+	require.True(t, dict.Has([]byte("k0")))
+	require.True(t, dict.Has([]byte("k1")))
+	require.Equal(t, "v0", dict.GetDefault([]byte("k0"), ""))
+	require.Equal(t, "v1", dict.GetDefault([]byte("k1"), ""))
+
+	dict.UpsertBatch([]byte("k2"), "v2a")
+	dict.UpsertBatch([]byte("k4"), "v4")
+	dict.UpsertBatch([]byte("k3"), "v3")
+	dict.UpsertBatch([]byte("k2"), "v2b")
+
+	require.Equal(t, "v0", dict.GetDefault([]byte("k0"), ""))
+	require.Equal(t, "v1", dict.GetDefault([]byte("k1"), ""))
+	require.Equal(t, "v2b", dict.GetDefault([]byte("k2"), ""))
+	require.Equal(t, "v3", dict.GetDefault([]byte("k3"), ""))
+	require.Equal(t, "v4", dict.GetDefault([]byte("k4"), ""))
+}
