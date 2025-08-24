f70a45f7fc298ea4e21daf2de0083edbb9cc3069
stergiotis <p@stergiotis.ch>
2025-08-24 22:30:48 +0200

    feat: vcs readout

--- public/observability/vcs/vsc.go
+++ public/observability/vcs/vsc.go
@@ -3,10 +3,41 @@ package vcs
 import (
 	"runtime/debug"
 	"strings"
+
+	"github.com/stergiotis/boxer/public/observability/eh"
 )
 
 const noBuildInfo = "<no-build-info-available>"
 
+func GetVcsRevision() (revision string, modified bool, err error) {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		err = eh.Errorf("no build information available")
+		return
+	}
+	found := 0
+	for _, setting := range info.Settings {
+		switch setting.Key {
+		case "vcs.revision":
+			revision = setting.Value
+			found++
+			if found == 2 {
+				return
+			}
+			break
+		case "vcs.modified":
+			modified = setting.Value == "true"
+			found++
+			if found == 2 {
+				return
+			}
+			break
+		}
+	}
+	return
+}
+
+// BuildVersionInfo Human readable build info (vcs fields)
 func BuildVersionInfo() string {
 	info, ok := debug.ReadBuildInfo()
 	if !ok {
