a6c2795516f72263952cb67b368d258a13f66bf3
stergiotis <p@stergiotis.ch>
2025-09-01 22:43:02 +0200

    fix: path determination

--- public/semistructured/leeway/ddl/clickhouse/lw_ddl_clickhouse_testutils.go
+++ public/semistructured/leeway/ddl/clickhouse/lw_ddl_clickhouse_testutils.go
@@ -9,15 +9,9 @@ import (
 func GetClickHouseBinaryPath() (path string, err error) {
 	var found bool
 	path, found = os.LookupEnv("clickhouse")
-	if found {
-		return
+	if !found {
+		path = os.ExpandEnv("$HOME/opt/clickhouse/clickhouse")
 	}
-
-	path = os.ExpandEnv("$HOME/opt/clickhouse/clickhouse")
-	if path != "" {
-		return
-	}
-	err = eh.Errorf("unable to locate clickhouse binary")
 
 	_, err = os.Stat(path)
 	if err != nil {
