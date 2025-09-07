8ebc9ded9b1b0f09018a1961393a7bbd7139e582
stergiotis <p@stergiotis.ch>
2025-09-07 21:17:51 +0200

    feat: indirect error handling support code

--- public/observability/eh/indirect.go
+++ public/observability/eh/indirect.go
@@ -1,1 +1,26 @@
 package eh
+
+import "errors"
+
+func AppendError(errsIn []error, err error) (errsOut []error) {
+	if err == nil {
+		errsOut = errsIn
+		return
+	}
+	l := len(errsIn)
+	if l == 0 {
+		errsOut = append(errsIn, err)
+		return
+	}
+	if errsIn[l-1] != err {
+		errsOut = append(errsIn, err)
+	}
+	return
+}
+func CheckErrors(errs []error) (err error) {
+	if len(errs) > 0 {
+		err = errors.Join(errs...)
+		return
+	}
+	return
+}
