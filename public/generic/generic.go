e940c06c774a2bd35efe6560ef99355f763d054d
stergiotis <p@stergiotis.ch>
2025-09-09 20:38:16 +0200

    feat: generic helpers

---
+++ public/generic/generic.go
@@ -0,0 +1,17 @@
+package generic
+
+import "reflect"
+
+// IsNil source https://stackoverflow.com/questions/73956346/how-to-check-if-generic-type-is-nil
+func IsNil[T any](t T) bool {
+	v := reflect.ValueOf(t)
+	kind := v.Kind()
+	// Must be one of these types to be nillable
+	return (kind == reflect.Ptr ||
+		kind == reflect.Interface ||
+		kind == reflect.Slice ||
+		kind == reflect.Map ||
+		kind == reflect.Chan ||
+		kind == reflect.Func) &&
+		v.IsNil()
+}
