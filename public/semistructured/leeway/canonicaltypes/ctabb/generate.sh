95ee06f434acab7d4d40c8e8ff475b81ce62c98f
stergiotis <p@stergiotis.ch>
2025-10-18 11:54:52 +0200

    fix: package name

--- public/semistructured/leeway/canonicaltypes/ctabb/generate.sh
+++ public/semistructured/leeway/canonicaltypes/ctabb/generate.sh
@@ -2,5 +2,5 @@
 set -ev
 here=$(dirname "$(readlink -f "$BASH_SOURCE")")
 cd "$here"
-../../../../../boxer.sh leeway ct abbrevs --packageName "ctabb" --import "github.com/stergiotis/boxer/public/semistructured/leeway/canonicalTypes" --astPackage="canonicalTypes" > "$here/canonicalTypes_abbrevs.out.go"
+../../../../../boxer.sh leeway ct abbrevs --packageName "ctabb" --import "github.com/stergiotis/boxer/public/semistructured/leeway/canonicaltypes" --astPackage="canonicaltypes" > "$here/canonicalTypes_abbrevs.out.go"
 gofmt -w ./*.go
