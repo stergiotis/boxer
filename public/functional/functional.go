ea402d1455fc6f1b110b2cb2d02aef7a7df33180
stergiotis <p@stergiotis.ch>
2025-10-24 13:59:50 +0200

    feat: iterator util functions

--- public/functional/functional.go
+++ public/functional/functional.go
@@ -1,5 +1,7 @@
 package functional
 
+import "iter"
+
 // TranslateEmpty if s is the empty value (type specific) TranslateEmpty returns replacement
 func TranslateEmpty[T comparable](s T, replacement T) (r T) {
 	if s == r {
@@ -12,4 +14,32 @@ type InterfaceIsReferentialTransparentType bool
 
 type PromiseReferentialTransparentI interface {
 	PromiseToBeReferentialTransparent() (_ InterfaceIsReferentialTransparentType)
+}
+
+func IterLeftOnly[L, R any](seq iter.Seq2[L, R]) iter.Seq[L] {
+	return func(yield func(L) bool) {
+		for l, _ := range seq {
+			if !yield(l) {
+				return
+			}
+		}
+	}
+}
+func IterRightOnly[L, R any](seq iter.Seq2[L, R]) iter.Seq[R] {
+	return func(yield func(R) bool) {
+		for _, r := range seq {
+			if !yield(r) {
+				return
+			}
+		}
+	}
+}
+func IterInterchanged[L, R any](seq iter.Seq2[L, R]) iter.Seq2[R, L] {
+	return func(yield func(R, L) bool) {
+		for l, r := range seq {
+			if !yield(r, l) {
+				return
+			}
+		}
+	}
 }
