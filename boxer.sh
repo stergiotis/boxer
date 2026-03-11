1643af0d41573b349b1ea7768d3c162105c10885
stergiotis <p@stergiotis.ch>
2026-03-11 22:51:06 +0100

    fix: path

--- boxer.sh
+++ boxer.sh
@@ -3,12 +3,13 @@ set -e
 set -o pipefail
 here=$(dirname "$(readlink -f "$BASH_SOURCE")")
 cd "$here"
-appfile="$here/$(basename "$(mktemp)")"
+appfile=$(realpath "$here/$(basename "$(mktemp)")")
 cleanup() {
     rv=$?
     rm -f -- "$appfile"
     exit $rv
 }
 trap 'cleanup' EXIT
-go build -buildvcs=true -tags $(cat "$here/tags" | tr -d "\n") -o "$appfile" ./public/app 1>&2
+go build -buildvcs=true -tags "$(cat "$here/tags" | tr -d "\n")" -o "$appfile" ./public/app 1>&2
+cd -
 "$appfile" --logFormat console "$@"
